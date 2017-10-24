d62a4c804cc99546d6c384f20277e4868a2a8acf
Corey McCandless <crm1994@gmail.com>
2017-10-24 16:36:48 -0500

    tests: dice

--- dice.py
+++ dice.py
@@ -1,37 +1,22 @@
 from random import randint
-import re
+
 
 class Dice:
     def __init__(self, sides):
         self.sides = sides
+
     def roll(self):
-        return randint(1,self.sides)
+        return randint(1, self.sides)
+
     def __str__(self):
         return 'D{}'.format(self.sides)
+
     def __lt__(self, other):
         return self.sides < other.sides
-        
-# class D20(Dice):
-    # def __init__(self):
-        # super().__init__(20)
-        
-# class D10(Dice):
-    # def __init__(self):
-        # super().__init__(10)
-        
-# class D8(Dice):
-    # def __init__(self):
-        # super().__init__(8)
-        
-# class D6(Dice):
-    # def __init__(self):
-        # super().__init__(6)
-        
-# class D4(Dice):
-    # def __init__(self):
-        # super().__init__(4)
-        
+
+
 D20 = Dice(20)
+D12 = Dice(12)
 D10 = Dice(10)
 D8 = Dice(8)
 D6 = Dice(6)
--- tests.py
+++ tests.py
@@ -1,4 +1,22 @@
 import unittest
 
+import dice
+
+class DiceTest(unittest.TestCase):
+    def test_die(self, die):
+        for _ in range(100):
+            roll = die.roll()
+            self.assertGreaterEqual(roll, 1)
+            self.assertLessEqual(roll, die.sides)
+
+    def test_presets(self):
+        from dice import *
+        for die in [D4, D6, D8, D10, D12, D20]:
+            self.test_die(die)
+
+    def test_arbitrary_die(self):
+        die = Dice(7)
+        test_die(die)
+
 if __name__ == '__main__':
     unittest.main()
