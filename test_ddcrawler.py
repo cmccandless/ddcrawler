7576a18fbc684c0c8d07794a518d06ccbe4eade8
Corey McCandless <crm1994@gmail.com>
2017-10-25 12:13:43 -0500

    test_ddcrawler: improve labeling on validation tests

--- test_ddcrawler.py
+++ test_ddcrawler.py
@@ -5,6 +5,12 @@ from ddt import ddt, data, unpack
 from dice import (Dice, D4, D6, D8, D10, D12, D20)
 
 
+class AnnotatedList(list):
+    def __init__(self, label, *args):
+        super().__init__(args)
+        self.__name__ = label
+
+
 @ddt
 class DiceTest(unittest.TestCase):
     @data(D4, D6, D8, D10, D12, D20, Dice(7), Dice(['heads', 'tails']))
@@ -13,12 +19,12 @@ class DiceTest(unittest.TestCase):
             roll = die.roll()
             self.assertIn(roll, die)
 
-    @data((9, True),
-          (['rock', 'paper', 'scissors'], True),
-          ([D4, D6, D8], True),
-          ('invalid', False),
-          (2.3, False))
     @unpack
+    @data(AnnotatedList('D9', 9, True),
+          AnnotatedList('str_list', ['rock', 'paper', 'scissors'], True),
+          AnnotatedList('D_multi', [D4, D6, D8], True),
+          AnnotatedList('str', 'invalid', False),
+          AnnotatedList('float', 2.3, False))
     def test_valid_die(self, sides, is_valid):
         if is_valid:
             Dice(sides)
