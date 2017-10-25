d56fc395e2445c449e5572e4a455edc5daeb0393
Corey McCandless <crm1994@gmail.com>
2017-10-25 11:05:01 -0500

    test_ddcrawler: add dice sides validation tests

--- dice.py
+++ dice.py
@@ -3,14 +3,26 @@ from random import randint
 
 class Dice:
     def __init__(self, sides):
+        if isinstance(sides, list):
+            self.__name__ = 'D[{}]'.format(','.join(map(str, sides)))
+        elif isinstance(sides, int):
+            self.__name__ = 'D{}'.format(sides)
+        else:
+            raise ValueError('"{}" is not a valid sides format'.format(sides))
         self.sides = sides
-        self.__name__ = 'D{}'.format(sides)
+
+    def __iter__(self):
+        if isinstance(self.sides, list):
+            return iter(self.sides)
+        return iter(range(1, self.sides + 1))
 
     def roll(self):
+        if isinstance(self.sides, list):
+            return self.sides[randint(0, len(self.sides) - 1)]
         return randint(1, self.sides)
 
     def __str__(self):
-        return 'D{}'.format(self.sides)
+        return self.__name__
 
     def __lt__(self, other):
         return self.sides < other.sides
--- test_ddcrawler.py
+++ test_ddcrawler.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 import unittest
-from ddt import ddt, data
+from ddt import ddt, data, unpack
 
 from dice import (Dice, D4, D6, D8, D10, D12, D20)
 
 
 @ddt
 class DiceTest(unittest.TestCase):
-    @data(D4, D6, D8, D10, D12, D20, Dice(7))
+    @data(D4, D6, D8, D10, D12, D20, Dice(7), Dice(['heads', 'tails']))
     def test_die(self, die):
         for _ in range(100):
             roll = die.roll()
-            self.assertGreaterEqual(roll, 1)
-            self.assertLessEqual(roll, die.sides)
+            self.assertIn(roll, die)
+
+    @data((9, True),
+          (['rock', 'paper', 'scissors'], True),
+          ([D4, D6, D8], True),
+          ('invalid', False),
+          (2.3, False))
+    @unpack
+    def test_valid_die(self, sides, is_valid):
+        if is_valid:
+            Dice(sides)
+        else:
+            with self.assertRaises(ValueError):
+                Dice(sides)
 
 
 if __name__ == '__main__':
